4a58e6eafd67ccfe8c1fa03bc96a15721a0fcb54
montrinquite <rodaimontrinquite@gmail.com>
2026-08-20 19:21:38 +0800

    r

--- js2/main.js
+++ js2/main.js
@@ -7,13 +7,7 @@ const tblRecords = document.getElementById("tblRecords");
 let arrRecords = JSON.parse(localStorage.getItem("arrRecords")) || [];
 const tblTHsLabels = ["First Name", "Middle Name", "Last Name", "Age", "Action"];
 
-
-if(arrRecords.length == 0) {
-    document.getElementById("status").style.display = "inline";
-    document.getElementById("status").innerHTML = "No Records...";
-} else {
-    document.getElementById("status").style.display = "none";
-}
+iterateRecords();
 
 btnInsertUpdate.addEventListener("click", () => {
 
